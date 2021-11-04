8ddf7f25dc2781cdd3dfe0a881021ae1889b1d8a
joojimin <wnwlals22@naver.com>
2021-11-05 00:54:10 +0900

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-batch-guide
+
 스프링 배치 완벽 가이드 연습
---
+++ src/main/java/com/example/springbatchguide/SpringBatchGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchGuideApplication.class, args);
+    }
+
+}
